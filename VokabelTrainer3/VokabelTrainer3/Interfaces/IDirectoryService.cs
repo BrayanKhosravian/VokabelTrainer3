96a0a233b62915bd44f4710fdd7c8b1bc3ae436c
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2019-02-22 00:01:14 +0100

    added interfaced IDirectoryService.cs and IFileService.cs

---
+++ VokabelTrainer3/VokabelTrainer3/Interfaces/IDirectoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VokabelTrainer3.Interfaces
+{
+    interface IDirectoryService
+    {
+        void CreateDirectoryHirarchy();
+
+    }
+}
