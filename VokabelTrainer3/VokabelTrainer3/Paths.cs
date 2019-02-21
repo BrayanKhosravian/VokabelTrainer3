a5bfda8a31ebe6a99508d640d196003c1e249b73
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2019-02-21 23:55:44 +0100

    added static Paths.cs

---
+++ VokabelTrainer3/VokabelTrainer3/Paths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace VokabelTrainer3
+{
+    public static class Paths
+    {
+        private static readonly string _path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+        // Root Path from _path
+        public static string RootPath { get; } = Path.Combine(_path, "Vokabeln");
+
+        // Parent paths of root
+        public static string BasicVocabsPath { get; } = Path.Combine(RootPath, "Basic");
+        public static string AdvancedVocabsPath { get; } = Path.Combine(RootPath, "Advanced");
+
+        // Children paths from parent paths
+        public static string AtTheHotel { get; } = Path.Combine(BasicVocabsPath, "AtTheHotel");
+        public static string CarRental { get; } = Path.Combine(BasicVocabsPath, "CarRental");
+        public static string GettingStarted { get; } = Path.Combine(BasicVocabsPath, "GettingStarted");
+        public static string TheJourney { get; } = Path.Combine(BasicVocabsPath, "TheJourney");
+        public static string TravellingAbroad { get; } = Path.Combine(BasicVocabsPath, "TravellingAbroad");
+
+        public static string[] GetBasicVocabsPaths => new[] { AtTheHotel, CarRental, GettingStarted, TheJourney, TravellingAbroad };
+        // public static string[] GetAdvancedVocabsPaths => new[] { };
+    }
+}
