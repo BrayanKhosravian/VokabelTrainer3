1a1f676c8aa7d4acd2e3390b8e559061776dd5b3
Brayan Khosravian <35541212+BrayanKhosravian@users.noreply.github.com>
2019-02-21 23:59:18 +0100

    added static ContainerConfig.cs for IOC DI (Autofac) structure (no implementation)

---
+++ VokabelTrainer3/VokabelTrainer3/ContainerConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace VokabelTrainer3
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+
+            // register
+
+            return builder.Build();
+        }
+    }
+}
